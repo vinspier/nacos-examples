323faf042e29559d73cb0dcbc27d64a0564ab747
fxb <fanxb@haibaobaoxian.cn>
2020-09-26 22:08:01 +0800

    boot-discovery demo init

---
+++ nacos-boot-examples/nacos-boot-discovery/src/main/java/com/vinspier/nacos/boot/discovery/NacosBootDiscoveryApplication.java
@@ -0,0 +1,21 @@
+package com.vinspier.nacos.boot.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 大鱼网络科技有限公司 版权所有 © Copyright 2018<br>
+ *
+ * @Description: <br>
+ * @Project: <br>
+ * @CreateDate: Created in 2020/9/26 21:50 <br>
+ * @Author: <a href="fanxb@haibaobaoxian.cn">fxb</a>
+ */
+@SpringBootApplication
+public class NacosBootDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosBootDiscoveryApplication.class,args);
+    }
+
+}
